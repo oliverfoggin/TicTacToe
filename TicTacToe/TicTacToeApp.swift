50067c61448a68416643debe7f3dc5d90bb44fba
Oliver Foggin <oliver.foggin@sky.uk>
2021-02-09 20:58:45 +0000

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Foggin, Oliver (Developer) on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
